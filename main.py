2c78ce17dc1a9c4d6113970dd96d179b4fc223b6
davidaamm <77129387+davidaamm@users.noreply.github.com>
2021-11-03 13:09:41 -0600

    Actualización de archivos json y main.py
    Ultimas actualizaciones de archivos al día

--- main.py
+++ main.py
@@ -271,9 +271,9 @@ async def check_twitch_online_streamers():
     else:   
       await chnl.send("@everyone - {} inicio strim :  https://www.twitch.tv/{}".format(notification["user_login"],notification["user_login"]),embed=embed)
       
-
 if __name__ == "__main__":
   check_twitch_online_streamers.start()
+  
 
 @client.command()
 async def kiss(ctx,member: discord.Member):
@@ -282,21 +282,32 @@ async def kiss(ctx,member: discord.Member):
 
   # our test search
   search_term = "anime kiss gif"
+  pos = '0'
+  pos2 = '50'
+  pos3 = '100'
 
   # get the top 8 GIFs for the search term
-  r = requests.get(
-    "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
+  r = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s&pos=%s" % (search_term, apikey, lmt, pos))
+  r2 = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s&pos=%s" % (search_term, apikey, lmt, pos2))
+  r3 = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s&pos=%s" % (search_term, apikey, lmt, pos3))
 
   if r.status_code == 200:
     # load the GIFs using the urls for the smaller GIF sizes
-    busqueda = json.loads(r.content)
+    busqueda = json.loads(r.text)
+    busqueda2 = json.loads(r2.text)
+    busqueda3 = json.loads(r3.text)
+    ds = [busqueda, busqueda2, busqueda3]
+    d = {}
+    for k in busqueda.keys():
+      d[k] = tuple(d[k] for d in ds)
+    indice=random.randint(0, 2)
     numero=random.randint(0, 49)
-    gif=busqueda['results'][numero]['media'][0]['gif']['url']
+    gif=d['results'][indice][numero]['media'][0]['gif']['url']
 
   else:
     busqueda = None
   
-  embed=discord.Embed(description=ctx.author.mention + "le ha dado besito a "+ member.mention, color=discord.Colour.from_rgb(204, 152, 197))
+  embed=discord.Embed(description=ctx.author.mention + " le ha dado besito a "+ member.mention, color=discord.Colour.from_rgb(204, 152, 197))
   embed.set_image(url=gif)
   await ctx.send(embed=embed)
 
@@ -307,16 +318,27 @@ async def hug(ctx,member: discord.Member):
 
   # our test search
   search_term = "anime hug gif"
+  pos = '0'
+  pos2 = '50'
+  pos3 = '100'
 
   # get the top 8 GIFs for the search term
-  r = requests.get(
-    "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
+  r = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s&pos=%s" % (search_term, apikey, lmt, pos))
+  r2 = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s&pos=%s" % (search_term, apikey, lmt, pos2))
+  r3 = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s&pos=%s" % (search_term, apikey, lmt, pos3))
 
   if r.status_code == 200:
     # load the GIFs using the urls for the smaller GIF sizes
-    busqueda = json.loads(r.content)
+    busqueda = json.loads(r.text)
+    busqueda2 = json.loads(r2.text)
+    busqueda3 = json.loads(r3.text)
+    ds = [busqueda, busqueda2, busqueda3]
+    d = {}
+    for k in busqueda.keys():
+      d[k] = tuple(d[k] for d in ds)
+    indice=random.randint(0, 2)
     numero=random.randint(0, 49)
-    gif=busqueda['results'][numero]['media'][0]['gif']['url']
+    gif=d['results'][indice][numero]['media'][0]['gif']['url']
 
   else:
     busqueda = None
@@ -402,14 +424,17 @@ async def strimer(ctx):
   add=ctx.message.content
   split=add.split()
   func=split[1]
-  strimer=split[2].lower()
+  try:
+      strimer=split[2].lower()
+  except IndexError:
+      strimer=""    
+
   userid=ctx.author.id
   test = config['watchlist']
   if (strimer == 'hpavel82') or (strimer == 'sicaac') or (strimer == 'marf0n') or (strimer == 'tiranodiego') or (strimer == 'deividaam'):
     await ctx.send(ctx.author.mention + "el papucho que intentas agregar ya esta en la lista general")
   else:
     if func == 'add':
-
       if strimer in strimers:
         if userid in strimers[strimer]:
           await ctx.send(ctx.author.mention + " ya habias registrao a este usario en tus notificaciones")
@@ -440,14 +465,34 @@ async def strimer(ctx):
           await ctx.send(ctx.author.mention + strimer + " no esta registrao en tus notificaciones")
       else:
         await ctx.send(ctx.author.mention + " este strimer no esta registrao en la lista de notificaciones")
+    elif func == 'list':
+      embed=discord.Embed(title="Strimers registrados",description=ctx.author.mention + ' acá esta tu lista: ',color=discord.Colour.from_rgb(204, 152, 197))
+      key_list = list(strimers.keys())
+      val_list = list(strimers.values())
+      list_strimers=[]
+      for i in range(0,len(key_list)):
+        valor = val_list[i]
+        if userid in valor:
+          strimer = key_list[i]
+          list_strimers.append(strimer)
+      cantstrimers=len(list_strimers)
+      if cantstrimers == 0:
+        await ctx.author.send('No tienes strimers registrados')
+      else:
+        lista_final = ' | '.join(list_strimers)
+        embed.add_field(name = chr(173), value=lista_final, inline=False)
+        await ctx.author.send(embed=embed)
 
   if strimer in test:
     pass
   else:
-    test.append(strimer)
-    nuevo={'watchlist':test}
-    with open("config.json", "w") as config_file:
-      json.dump(nuevo, config_file)
+    if strimer == "":
+      pass
+    else:
+      test.append(strimer)
+      nuevo={'watchlist':test}
+      with open("config.json", "w") as config_file:
+        json.dump(nuevo, config_file)
 
 
 
@@ -461,6 +506,7 @@ async def help(ctx):
   embed.add_field(name="Twitch", value="Comandos de tuich",inline=False)
   embed.add_field(name="!strimer add usuario",value="Agrega al usuario de tuich a tus notificaciones, ejemplo de comando: __**!strimer add chismestrum**__", inline=True)
   embed.add_field(name="!strimer del usuario",value="Elimina al usuario de tuich de tus notificaciones, ejemplo de comando: __**!strimer del chismestrum**__", inline=True)
+  embed.add_field(name="!strimer list",value="Para ver la lista de strimers que has puesto en tus notificaciones", inline=True)
 
   embed.add_field(name="Amorts", value="Comandos de besitos, abracitos, gatitos y perritos ",inline=False)
   embed.add_field(name="!kiss",value="Manda un gif random de besito a la persona que quieras, ejemplo de comando: __**!kiss @chismestrum**__", inline=True)
@@ -469,7 +515,8 @@ async def help(ctx):
 
   embed.add_field(name="!sub", value="Comando para saber cuantos días han pasado desde que sicaac te ha regalado una sub, para agregar la fecha en la que sicaac te regale una sub utiliza el comando __**!sub add**__",inline=False)
 
-  await ctx.send(embed=embed)
+  await ctx.author.send(embed=embed)
 
 keep_alive()
 client.run(os.environ['TOKEN'])
+
